4aabec090e2c0b5ffebfb06271d7b80c6056064a
Encryptzen <ck2222@cse.jgec.ac.in>
2021-02-21 15:05:50 +0530

    first commit

---
+++ app/tests/test_urls.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from app.views import project_list, project_detail, ProjectCreateView
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_url_resolves(self):
+        url=reverse('list')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,project_list)
+
+    def test_project_create_url_resolves(self):
+        url=reverse('add')
+        self.assertEquals(resolve(url).func.view_class,ProjectCreateView)
+
+    def test_detail_url_resolves(self):
+        url=reverse('detail',args=['some-slugs'])
+        self.assertEquals(resolve(url).func, project_detail)
